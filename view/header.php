f10de2b5101b502f429e2602a50d9369613d4bdf
Snorlex9 <97varunkumar@gmail.com>
2023-04-15 11:08:32 +0800

    update header

--- view/header.php
+++ view/header.php
@@ -9,7 +9,7 @@
             .header {
                 
                 background: black;
-                color: yellow;
+                color: whitesmoke;
                 font-size: 20px;
                 width:100%;
                 height:50px; 
