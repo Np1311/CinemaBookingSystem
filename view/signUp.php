650a7db8802baf52edd1280f5a344e4699c9046a
Np1311 <130040190+Np1311@users.noreply.github.com>
2023-04-15 20:04:10 +0800

    update

--- view/signUp.php
+++ view/signUp.php
@@ -94,6 +94,20 @@ session_start();
                 <label for="dob">Date of Birth:</label>
                 <input type="date" id="dob" name="dob" required><br>
 
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required></br>
+
+                    
+                <input class="form-check-input" type="checkbox" onclick="myFunction('password')">&nbspShow Password</input></br></br>
+                   
+                <label for="confirm-password">Confirm Password:</label>
+                <input type="password" id="confirm-password" name="confirm-password" required></br>
+                    
+                <input  style="font-size: 10px;" class="form-check-input" type="checkbox" onclick="myFunction('confirm-password')"> &nbspShow Password</input>
+                
+                <div > <span id = 'password-message'></span></div>
+
+
                 <label for="membership">Membership Type:</label>
                 <select id="membership" name="membership">
                     <option value="basic">Basic</option>
@@ -104,5 +118,66 @@ session_start();
                 <input type="submit" value="Submit">
             </form>
         </div>
+        <script>
+            function myFunction(type) {
+            var x = document.getElementById(type);
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+            }
+            let slideIndex = 1;
+            showSlides(slideIndex);
+
+            function plusSlides(n) {
+            showSlides(slideIndex += n);
+            }
+
+            function currentSlide(n) {
+            showSlides(slideIndex = n);
+            }
+
+            function showSlides(n) {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            let dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {slideIndex = 1}    
+            if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";  
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";  
+            dots[slideIndex-1].className += " active";
+            }
+            const password = document.getElementById("password");
+            const confirmPassword = document.getElementById("confirm-password");
+            const passwordMessage = document.getElementById("password-message");
+
+            // Function to check if passwords match
+            function checkPasswords() {
+            if (password.value !== confirmPassword.value) {
+                passwordMessage.innerHTML = "Passwords do not match.";
+            } else {
+                passwordMessage.innerHTML = "";
+            }
+            }
+
+            // Listen for changes to the password and confirm password fields
+            password.addEventListener("input", checkPasswords);
+            confirmPassword.addEventListener("input", checkPasswords);
+
+            const form = document.querySelector('form');
+            form.addEventListener('submit', (event) => {
+                // Prevent form submission if passwords do not match
+                if (password.value !== confirmPassword.value) {
+                    event.preventDefault();
+                    passwordMessage.innerHTML = "Passwords do not match.";
+                }
+            });
+        </script>
     </body>
 </html>
