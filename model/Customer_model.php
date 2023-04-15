daabf9c3c7e984cc7b7bd9510e1e6423d051e1c2
Np1311 <130040190+Np1311@users.noreply.github.com>
2023-04-15 12:59:23 +0800

    Update Customer_model table for customer with id, fname, lname, seat_row, seat_coloun, phone, email, pass, dob

--- model/Customer_model.php
+++ model/Customer_model.php
@@ -1,19 +1,30 @@
 <?php
-$conn = ;
+$conn = new mysqli('localhost','root', '');
 class customer{
-    public function createTable($tableName){
+    public function createTable(){
         global $conn;
-        $conn -> select_db("CSIT314Try");
+        $conn -> select_db("CSIT314_Test");
         $sql = "CREATE TABLE IF NOT EXISTS `customer` (
-            `staff_id` varchar(5) NOT NULL,
-            `id` int(11) NOT NULL DEFAULT 0
+            `customer_id` int NOT NULL AUTO_INCREMENT,
+            `fname` varchar(128) NOT NULL,
+            `lname` varchar(128) NOT NULL,
+            `seat_row` int NOT NULL DEFAULT 0,
+            `seat_column`INT NOT NULL DEFAULT 0,
+            `phone` INT NOT NULL DEFAULT 0,
+            `email` VARCHAR(128) NOT NULL,
+            `password` VARCHAR(128) NOT NULL,
+            `dob` DATE NOT NULL,
+            PRIMARY KEY(customer_id)
           );";
     
           if ($conn->query($sql) === TRUE) {
-              echo "Table subject created successfully";
+              echo "Table customer created successfully";
           } else {
               echo "Error creating table: " . $conn->error;
           }
     }
 }
+
+$con = new customer;
+$con -> createTable();
 ?>
