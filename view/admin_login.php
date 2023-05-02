0d7f6a361419783ebd80320e2b571babbbcab11c
Np1311 <130040190+Np1311@users.noreply.github.com>
2023-05-02 13:55:48 +0800

    update

--- view/admin_login.php
+++ view/admin_login.php
@@ -100,7 +100,21 @@ require('header_login.html')
         }
     </script>
     <?php
-    require('login_boundary.php');
+    require('../controller/login_controller.php');
+    if (isset($_POST['submit'])){
+        $phone = $_POST['phone'];
+        $pass = $_POST['pass'];
+        if(isset($_SESSION['profile'])== null){
+            $_SESSION['profile'] = 'customer';
+        }
+        echo $_SESSION['profile'];
+        
+        $controller = new login_controller();
+        
+        $controller->validateLogin($_SESSION['profile'],$phone,$pass);
+            
+        
+    }
     ?>
 </body>
 </html>
