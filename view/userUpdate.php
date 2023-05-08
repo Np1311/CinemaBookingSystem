6cbee11064a54b597b2cc1d74d2631454a51ae01
jooo576 <jonathan.chen72@gmail.com>
2023-05-08 13:12:29 +0800

    userupdate

--- view/userUpdate.php
+++ view/userUpdate.php
@@ -4,46 +4,7 @@ $curProfile = $_GET['curProfile'];
 $userID = $_GET['updateID'];
 $userAccount = new user_update_controller;
 $arr = $userAccount -> showUpdate($curProfile,$userID);
-echo "<div class='profile'>";
-echo "<h1>Update Profile</h1>";
-echo "<form  method='post'>";
-    echo "<label for='first_name'>First Name:</label>";
-    echo "<input type='text' name='first_name' id='first_name' value=".$arr['fname']."><br><br>";
-    echo '<label for="last_name">Last Name:</label>';
-    echo "<input type='text' name='last_name' id='last_name' value=".$arr['lname']."><br><br>";
-    echo '<label for="email">Email:</label>';
-    echo "<input type='email' name='email' id='email' value=".$arr['email']."><br><br>";
-    echo '<label for="date_of_birth">Date of Birth:</label>';
-    echo "<input type='date' name='date_of_birth' id='date_of_birth' value=".$arr['dob']."><br><br>";
-    echo '<label for="phone">Phone Number:</label>';
-    echo "<input type='text' name='phone' id='phone' value=".$arr['phone']."><br><br>";
-    echo '<label for="status">Status:</label>';
-    echo '<select class="form-control" name="status">';
-        if($arr['status'] == 'active') {
-            echo '<option value="active" SELECTED> active </option>';
-            echo '<option value="suspend" > suspend </option>';
-        } else{
-            echo '<option value="active"> active </option>';
-            echo '<option value="suspend" SELECTED> suspend </option>';
-        }
-            
-    echo'</select><br><br>';
-    echo '<input type="submit" name="submit" value="Save Changes">';
-echo "</form>";
-echo "</div>";
-if(isset($_POST['submit'])){
-    $fname = $_POST['first_name'];
-    $lname = $_POST['last_name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $dob = $_POST['date_of_birth'];
-    $status = $_POST['status'];
-    if($userAccount ->validateUser($curProfile,$fname,$lname,$phone,$email,$dob,$status,$userID)){
-        
-        echo" <script>window.location='../view/admin_home_view.php';</script>";
-    }
 
-}
 
 // print_r($curProfile);
 // print_r($userID);
@@ -55,14 +16,24 @@ if(isset($_POST['submit'])){
 <html>
     <head>
     <style>
-        .profile {
+        /* .profile {
             margin: 0 auto;
             max-width: 500px;
             padding: 20px;
             background-color: #f7f7f7;
             border-radius: 5px;
             box-shadow: 0px 0px 5px rgba(0,0,0,0.3);
+        } */
+
+        .profile {
+            margin: 0 auto;
+            max-width: 500px;
+            padding: 20px;
+            background-color: #e7dbd0; /* set background color here */
+            border-radius: 5px;
+            box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
         }
+
 
         h1 {
             font-size: 24px;
@@ -70,6 +41,12 @@ if(isset($_POST['submit'])){
             text-align: center;
         }
 
+        body {
+  background-color: #e7dbd0;
+  height: 100%;
+}
+
+        
         label {
             display: block;
             font-weight: bold;
@@ -91,8 +68,9 @@ if(isset($_POST['submit'])){
         }
 
         input[type="submit"] {
-            background-color: #007bff;
-            color: #fff;
+            background-color: white;
+            color: #bd9a7a;
+            border: 2px solid;
             border: none;
             padding: 10px 20px;
             font-size: 16px;
@@ -102,18 +80,58 @@ if(isset($_POST['submit'])){
         }
 
         input[type="submit"]:hover {
-            background-color: #0062cc;
+            background-color: #FFFFFF; 
+            color: #0a0805;
         }
+
+        
+        
     </style>
     </head>
     <body>
-        <!-- 
         <?php
-        // $user = $_SESSION['profileInfo'];
-        // $dob = DateTime::createFromFormat('d/m/Y', $arr['dob']);
-        // $formatted_dob = $dob->format('Y-m-d');
+        echo "<div class='profile'>";
+        echo "<h1>Update Profile</h1>";
+        echo "<form  method='post'>";
+            echo "<label for='first_name'>First Name:</label>";
+            echo "<input type='text' name='first_name' id='first_name' value=".$arr['fname']."><br><br>";
+            echo '<label for="last_name">Last Name:</label>';
+            echo "<input type='text' name='last_name' id='last_name' value=".$arr['lname']."><br><br>";
+            echo '<label for="email">Email:</label>';
+            echo "<input type='email' name='email' id='email' value=".$arr['email']."><br><br>";
+            echo '<label for="date_of_birth">Date of Birth:</label>';
+            echo "<input type='date' name='date_of_birth' id='date_of_birth' value=".$arr['dob']."><br><br>";
+            echo '<label for="phone">Phone Number:</label>';
+            echo "<input type='text' name='phone' id='phone' value=".$arr['phone']."><br><br>";
+            echo '<label for="status">Status:</label>';
+            echo '<select class="form-control" name="status">';
+                if($arr['status'] == 'active') {
+                    echo '<option value="active" SELECTED> active </option>';
+                    echo '<option value="suspend" > suspend </option>';
+                } else{
+                    echo '<option value="active"> active </option>';
+                    echo '<option value="suspend" SELECTED> suspend </option>';
+                }
+                    
+            echo'</select><br><br>';
+            echo '<input type="submit" name="submit" value="Save Changes">';
+        echo "</form>";
+        echo "</div>";
+        if(isset($_POST['submit'])){
+            $fname = $_POST['first_name'];
+            $lname = $_POST['last_name'];
+            $email = $_POST['email'];
+            $phone = $_POST['phone'];
+            $dob = $_POST['date_of_birth'];
+            $status = $_POST['status'];
+            if($userAccount ->validateUser($curProfile,$fname,$lname,$phone,$email,$dob,$status,$userID)){
+                
+                echo" <script>window.location='../view/admin_home_view.php';</script>";
+            }
+        
+        }
         ?>
-        <div class="profile">
+        <!-- <div class="profile">
             <h1>Update Profile</h1>
             <form  method="post">
             
@@ -129,6 +147,6 @@ if(isset($_POST['submit'])){
             <input type="text" name="phone" id="phone" value="<?php //echo $user['phone']; ?>"><br><br>
             <input type="submit" name="submit" value="Save Changes">
             </form>
-        </div> -->
+        </div>  -->
     </body>
 </html>
