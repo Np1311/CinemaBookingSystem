2030d6a9901a5998ef9170f7a44085eedbc328e6
Np1311 <130040190+Np1311@users.noreply.github.com>
2023-05-09 13:21:00 +0800

    update

--- controller/signUp_controller.php
+++ controller/signUp_controller.php
@@ -1,10 +1,10 @@
 <?php
-require ('../model/user_model.php');
+include_once ('../model/user_model.php');
 
 $model = new user;
 //require ('../view/header.php');
 class SignUpController{
-    
+
     public function createAccount($profile,$fname,$lname,$phone,$email,$password,$dob){
         global $model;
         if($model->createUser($profile,$fname,$lname,$phone,$email,$password,$dob)){
