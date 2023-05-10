528ea817e18b8557900c6c51b4dac2b135bbff71
Np1311 <130040190+Np1311@users.noreply.github.com>
2023-05-10 23:51:52 +0800

    Update customer_booking.php

--- view/customer/customer_booking.php
+++ view/customer/customer_booking.php
@@ -28,7 +28,7 @@ $column = $array['totalColumn'];
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
-    <link rel="icon" href="logo.png">
+    <link rel="icon" href="cinemaLogo.png">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
