9be565fd4115c0d828a0cc4eb93c9d234c96f8bd
jooo576 <jonathan.chen72@gmail.com>
2023-05-10 17:46:10 +0800

    update

--- view/userUpdate.php
+++ view/userUpdate.php
@@ -17,30 +17,26 @@ $arr = $userAccount -> showUpdate($curProfile,$userID);
 <html>
     <head>
     <style>
-        /* .profile {
-            margin: 0 auto;
-            max-width: 500px;
-            padding: 20px;
-            background-color: #f7f7f7;
-            border-radius: 5px;
-            box-shadow: 0px 0px 5px rgba(0,0,0,0.3);
-        } */
 
-        .profile {
-            margin: 0 auto;
-            max-width: 500px;
-            padding: 20px;
-            background-color: #e7dbd0; /* set background color here */
-            border-radius: 5px;
-            box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
-        }
-
+    .profile {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center; /* Center the form vertically */
+        margin: 0 auto;
+        max-width: 500px;
+        padding: 20px;
+        background-color: #ffffff; /* Set the background color to white */
+        border-radius: 5px;
+        box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
+    }
 
         h1 {
             font-size: 24px;
             margin-bottom: 20px;
             text-align: center;
         }
+
 
         body {
         background-color: #e7dbd0 !important;
@@ -68,7 +64,7 @@ $arr = $userAccount -> showUpdate($curProfile,$userID);
             height: 40px;
         }
 
-        input[type="submit"] {
+         input[type="submit"] {
             background-color: #bd9a7a;
             color: #fff;
             padding: 10px 20px;
@@ -82,71 +78,81 @@ $arr = $userAccount -> showUpdate($curProfile,$userID);
                 background-color: #fff;
                 color: #bd9a7a;
                 border: 2px solid;
-        }
+        } 
 
+            button {
+                background-color: #bd9a7a;
+                color: #fff;
+                padding: 10px 20px;
+                border: none;
+                border-radius: 5px;
+                cursor: pointer;
+                transition: background-color 0.3s;
+            }
+            
+            button:hover {
+                background-color: #fff;
+                color: #bd9a7a;
+                border: 2px solid;
+            } 
+            .button-container {
+                display: flex;
+                justify-content: space-between;
+                width: 100%;
+            }
+
+            .button-container button {
+                flex-basis: 48%; /* Adjust the width as needed */
+            }
         
         
     </style>
     </head>
     <body>
-        <?php
-        echo "<div class='profile'>";
-        echo "<h1>Update Profile</h1>";
-        echo "<form  method='post'>";
-            echo "<label for='first_name'>First Name:</label>";
-            echo "<input type='text' name='first_name' id='first_name' value=".$arr['fname']."><br><br>";
-            echo '<label for="last_name">Last Name:</label>';
-            echo "<input type='text' name='last_name' id='last_name' value=".$arr['lname']."><br><br>";
-            echo '<label for="email">Email:</label>';
-            echo "<input type='email' name='email' id='email' value=".$arr['email']."><br><br>";
-            echo '<label for="date_of_birth">Date of Birth:</label>';
-            echo "<input type='date' name='date_of_birth' id='date_of_birth' value=".$arr['dob']."><br><br>";
-            echo '<label for="phone">Phone Number:</label>';
-            echo "<input type='text' name='phone' id='phone' value=".$arr['phone']."><br><br>";
-            echo '<label for="status">Status:</label>';
-            echo '<select class="form-control" name="status">';
-                if($arr['status'] == 'active') {
-                    echo '<option value="active" SELECTED> active </option>';
-                    echo '<option value="suspend" > suspend </option>';
-                } else{
-                    echo '<option value="active"> active </option>';
-                    echo '<option value="suspend" SELECTED> suspend </option>';
-                }
-                    
-            echo'</select><br><br>';
-            echo '<input type="submit" name="submit" value="Save Changes">';
-        echo "</form>";
-        echo "</div>";
-        if(isset($_POST['submit'])){
-            $fname = $_POST['first_name'];
-            $lname = $_POST['last_name'];
-            $email = $_POST['email'];
-            $phone = $_POST['phone'];
-            $dob = $_POST['date_of_birth'];
-            $status = $_POST['status'];
-            if($userAccount ->validateUser($curProfile,$fname,$lname,$phone,$email,$dob,$status,$userID)){
-                
-                echo" <script>window.location='../view/admin_home_view.php';</script>";
-            }
-        
+    <?php
+    echo "<div class='profile'>";
+    echo "<h1>Update Profile</h1>";
+    echo "<form method='post'>";
+    echo "<label for='first_name'>First Name:</label>";
+    echo "<input type='text' name='first_name' id='first_name' value=".$arr['fname']."><br><br>";
+    echo '<label for="last_name">Last Name:</label>';
+    echo "<input type='text' name='last_name' id='last_name' value=".$arr['lname']."><br><br>";
+    echo '<label for="email">Email:</label>';
+    echo "<input type='email' name='email' id='email' value=".$arr['email']."><br><br>";
+    echo '<label for="date_of_birth">Date of Birth:</label>';
+    echo "<input type='date' name='date_of_birth' id='date_of_birth' value=".$arr['dob']."><br><br>";
+    echo '<label for="phone">Phone Number:</label>';
+    echo "<input type='text' name='phone' id='phone' value=".$arr['phone']."><br><br>";
+    echo '<label for="status">Status:</label>';
+    echo '<select class="form-control" name="status">';
+    if ($arr['status'] == 'active') {
+        echo '<option value="active" SELECTED> active </option>';
+        echo '<option value="suspend"> suspend </option>';
+    } else {
+        echo '<option value="active"> active </option>';
+        echo '<option value="suspend" SELECTED> suspend </option>';
+    }
+    echo '</select><br><br>';
+    
+    echo "<div class='button-container'>";
+    echo "<input type='submit' name='submit' value='Save Changes'>";
+    echo "<button type='button' onclick=\"window.location.href = 'admin_home_view.php'\">Back</button>";
+    echo "</div>";
+    
+    echo "</form>";
+    echo "</div>";
+    
+    if (isset($_POST['submit'])) {
+        $fname = $_POST['first_name'];
+        $lname = $_POST['last_name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $dob = $_POST['date_of_birth'];
+        $status = $_POST['status'];
+        if ($userAccount->validateUser($curProfile, $fname, $lname, $phone, $email, $dob, $status, $userID)) {
+            echo " <script>window.location='../view/admin_home_view.php';</script>";
         }
-        ?>
-        <!-- <div class="profile">
-            <h1>Update Profile</h1>
-            <form  method="post">
-            
-            <label for="first_name">First Name:</label>
-            <input type="text" name="first_name" id="first_name" value="<?php //echo $user['first_name']; ?>"><br><br>
-            <label for="last_name">Last Name:</label>
-            <input type="text" name="last_name" id="last_name" value="<?php //echo $user['last_name']; ?>"><br><br>
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email" value="<?php //echo $user['email']; ?>"><br><br>
-            <label for="date_of_birth">Date of Birth:</label>
-            <input type="date" name="date_of_birth" id="date_of_birth" value="<?php //echo $dob; ?>"><br><br>
-            <label for="phone">Phone Number:</label>
-            <input type="text" name="phone" id="phone" value="<?php //echo $user['phone']; ?>"><br><br>
-            <input type="submit" name="submit" value="Save Changes">
-            </form>
-        </div>  -->
-    </body>
+    }
+    ?>
+</body>
 </html>
