8f10d792c7c9e5810d50272857f927dd54ae689f
jooo576 <jonathan.chen72@gmail.com>
2023-05-10 12:35:23 +0800

    update

--- view/admin_search_account.php
+++ view/admin_search_account.php
@@ -14,7 +14,7 @@ form {
     max-width: 1000px; /*New things */
     margin: 0 auto; /* Set left and right margin to auto */
     background-color: #fff;
-    padding: 40px;
+    padding: 20px;
     border-radius: 10px;
     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
     position: absolute; /* Position the form absolutely */
