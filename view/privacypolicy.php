ef34a3904838912a96456e11dfac755634207685
jooo576 <jonathan.chen72@gmail.com>
2023-05-14 03:26:50 +0800

    update

---
+++ view/privacypolicy.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<?php
+require ('header.html');
+?>
+<html>
+    <head>
+        <style>
+        h1 {
+            color: #FF0000;
+        }
+        
+        hr {
+            border: solid 3px;
+        }
+        
+        strong {
+            font-weight: bold;
+        }
+        
+        .btn-primary {
+            background-color: #bd9a7a;
+            border: 2px solid #bd9a7a;
+            color: white;
+            padding: 15px;
+            text-align: center;
+            text-decoration: none;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 16px;
+            border-radius: 8px;
+            width: 400px;
+            margin: 0 auto; /* Center align the button horizontally */
+        }
+        
+        .btn-primary:hover {
+            background-color: #fff;
+            color: #bd9a7a;
+            border: 2px solid;
+        }
+
+        body {
+            background-color: #e7dbd0;
+            margin-top : 100px;
+        }
+        </style>
+    </head>
+    <body>
+    <h1>Privacy Policy</h1>
+<p>Your privacy is very important to us. We are committed to protecting the personal data that you disclose to us in accordance with the Personal Data Protection Act 2012.
+We will not disclose or sell your personal data to third parties for their marketing purposes without your express consent. 
+Please read this privacy policy to learn more about the ways in which we collect, use and disclose your personal data.</p>
+<hr>
+<h1>Overview</h1>
+<p>We have prepared this Privacy Policy (“Policy”) because we believe you should know as much as possible about our practices on the collection, use and disclosure of personal data so that you can make informed decisions when using this website that we may make available from time to time. By visiting this website, you agree to be bound by the terms and conditions of this Policy. If you do not agree, please do not use or access this website.</p>
+<p>This Policy describes the information that we collect from you through this website and how the information would be used and disclosed.</p>
+<p>In this Policy, “personal data” means any data, whether true or not, about an individual who can be identified (a) from that data; or (b) from that data and other information to which we have or are likely to have access, including data in our records as may be updated from time to time.</p>
+<p>By using this website, you will be deemed to have consented to our collection, use and disclosure of your personal data in accordance with this Policy. This Policy is incorporated into and subject to the terms of the Capybara Cinema User Agreement</p>
+<hr>
+<h1>TABLE OF CONTENTS</h1>
+<ol>
+    <li>INFORMATION WE COLLECT</li>
+    <p>Our primary purpose in collecting personal data is to provide you with a safe, smooth, efficient, and customized experience when you use this website. This allows us to provide services and features that most likely meet your needs, and to customize our service to make your experience safer and easier. We only collect personal data about you that we consider necessary for achieving this purpose.</p>
+    <p>If you choose to use the services on this website, we may require you to provide your contact and identity information and other personal information as indicated on the relevant forms. Where possible, we will indicate which fields are mandatory and which fields are optional. You will always have the option not to provide the information requested by choosing not to use a particular service or feature.</p>
+    <p>In some situations, we may require you to provide financial information. We use your financial information, including credit card information, to verify the accuracy of your name, address, and other information, as well as to bill you for your use of our services.</p>
+    <li>COOKIE MANAGEMENT</li>
+    <p>We use cookies to automatically track certain information based upon your behavior on the website. Cookies are small pieces of information which are issued to your computer or mobile device when you visit this website to store and track information about your use of this website. We use this information to do internal research on our users' demographics, interests, and behavior to better understand, protect and serve you and our community. This information may include the URL that you visited prior to visiting this website, which URL you visited next, your computer browser information, and your IP address</p>
+    <p>Unless you have chosen to remain anonymous through your device and/or the web browser, the above-mentioned information may be collected and used automatically through your use of our website.</p>
+    <p>Additionally, we may share non-personally identifiable information with third parties, such as location data, advertising identifiers, or a cryptographic hash of a common account identifier, to facilitate the display of targeted advertising.</P>
+    <p>Although most browsers and devices accept cookies by default, their settings usually allow you to clear or decline cookies. If you disable cookies, however, some of the features of our website may not function properly.</p>
+    <p>There are options available to control or limit how we and our partners use cookies and similar technologies, including for advertising. You can use the following methods to manage Cookies. In order to do so, you must take such steps on each browser or device that you use. If you replace, change, or upgrade your browser or device, or if you delete your Cookies, you may need to use these opt-out tools again.</p>
+    <p>To opt out of ads on Facebook or Google that are targeted to your interests, use your Facebook or Google Ads settings.</p>
+    <p>If you are using your mobile device, check the device for settings that control ads based on your interactions with the applications on your device. For example, on your iOS device, enable the "Limit Ad Tracking" setting, and on your Android device, enable the "Opt out of Ads Personalization" setting.</p>
+    <p>Our websites do not respond to Do Not Track signals because we do not track our users over time and across third-party websites to provide targeted advertising. However, we believe that you should have a choice regarding interest-based ads served by our partners, which is why we outline the options available to you above.</p>
+    <li>OUR USE OF YOUR INFORMATION</li>
+    <p>We use your personal data to facilitate the services you request. We also use your personal data with other information that we have collected from your current and past activities on the website to: attend to your queries; troubleshoot technical problems; help promote safe online transactions; collect payments; measure consumer interest in our products and services, inform you about online and offline offers, products, services, and updates; customize your experience; detect and protect us against error, fraud, and other criminal activity; and as otherwise described to you at the time of collection. At times, we may look across multiple users to identify problems or resolve disputes, and in particular, we may examine your personal data to identify users using multiple User IDs or aliases. We may compare and review your personal data for errors, omissions, and accuracy.</p>
+    <p>You agree that we may use your personal data for the aforementioned purposes, which are intended to improve our marketing and promotional efforts, analyze site usage, improve our content and product offerings, and customize our online content, layout, and services. Such use of your personal data will enable us to constantly improve our website and tailor it to meet your needs, providing you with a smooth, efficient, safe, and customized experience while using the website.</p>
+</ol>
+<button class="btn-primary" type="button" onclick="window.location.href = 'index.php'">Back</button>
+</body>
+</html>
