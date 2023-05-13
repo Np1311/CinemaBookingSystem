3e002dc649e469c9807659c9c975ba760592cedc
jooo576 <jonathan.chen72@gmail.com>
2023-05-14 03:39:36 +0800

    update

---
+++ view/termofuse.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<?php
+require ('header.html');
+?>
+<html>
+    <head>
+        <style>
+        h1 {
+            color: #FF0000;
+        }
+        
+        hr {
+            border: solid 3px;
+        }
+        
+        strong {
+            font-weight: bold;
+        }
+        
+        .btn-primary {
+            background-color: #bd9a7a;
+            border: 2px solid #bd9a7a;
+            color: white;
+            padding: 15px;
+            text-align: center;
+            text-decoration: none;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 16px;
+            border-radius: 8px;
+            width: 400px;
+            margin: 0 auto; /* Center align the button horizontally */
+        }
+        
+        .btn-primary:hover {
+            background-color: #fff;
+            color: #bd9a7a;
+            border: 2px solid;
+        }
+
+        body {
+            background-color: #e7dbd0;
+            margin-top : 100px;
+        }
+        </style>
+    </head>
+    <body>
+    <h1>Terms of Service</h1>
+<hr>
+<h3>Prices</h3>
+<p>All our prices are nett over at our websites and GST is included</p>
+<hr>
+<h3>PAYMENT PROCESSING</h3>
+<p>We accept VISA and MASTERCARD debit and credit card payments. We ensure accuracy in our billing as all our transactions are automatically processed.</p>
+<hr>
+<h3>CANCELLATION OF ORDERS and REFUND POLICY</h3>
+<p>We shall be entitled to cancel or terminate our obligations to fulfill any order, for any reason whatsoever, even after an order is confirmed by, and paid for by the customer, with or without notice and we shall not be liable to any party for such termination or cancellation.
+In such event, we shall refund to the customer the ticket price.</p>
+<p>We will not entertain any cancellations by the customer once the order is confirmed by, and paid for by the customer.</P>
+<hr>
+<h3>EXCHANGE & REFUNDS</h3>
+<p>All tickets, Food & Beverages Combos, Merchandises and Gift Cards sold are non-exchangeable and non-refundable.</P>
+<hr>
+<h3>By purchasing our Golden Village Tickets, you had agreed on the following Terms & Conditions :</h3>
+<ol>
+<li>You agree that purchased tickets may be used by the Ticket Holder only to gain admission to the relevant movie screenings, events and not for any other purpose.</li>
+<li>You agree that all tickets, whether purchased or complimentary, may not, without the prior written consent of Capybara Cinema, be resold or offered for resale at a premium or otherwise (whether by online auction sites or otherwise) or be used for or in connection with advertising, promotion or other commercial purposes (including and without limitation to competitions and trade promotions) or to enhance the demand for other goods or services, either by the original buyer or any subsequent bearer. You further agree that if a ticket is sold or used in breach of this condition, the ticket may be cancelled without a refund, and the bearer of the ticket may be refused admission.</li>
+<li>You agree entry may be refused if a ticket is damaged, defaced, forged or copied in any way or is not bought from any of Capybara Cinema's locations, Capybara Cinema's website, Ticketing Kiosks, or other authorized points of sale.</li>
+<li>You agree that Capybara Cinema shall not be liable for any loss and/or fraudulent cases arising from the purchase of tickets from any unauthorized points of sale.</li>
+<li>You agree to abide by the House Rules set by Capybara Cinema when you patronize Capybara Cinema's cinemas. The House Rules are displayed prominently in the vicinity of the box office and the entrance to the Capybara Cinema's cinemas.</li>
+<li>You agree that Capybara Cinema reserves the right to refuse entry to you or to remove you from Capybara Cinema's premises if you do not comply with the House Rules and/or at its discretion without giving any reason whatsoever. In such an event, Capybara Cinema reserves the right to refund the price of tickets at its own discretion without giving any reason whatsoever.</li>
+<li>You agree that Capybara Cinema reserves the right to change or cancel the show times of any movie at any time without prior notice to you and that Capybara Cinema shall not be liable to you or any third party for such cancellation or change. In such an event, Capybara Cinema shall refund to you an amount not exceeding the ticket price.</li>
+<li>You agree that Capybara Cinema shall not be liable to you for any direct, indirect and/or consequential losses, damages, injuries suffered by you within Capybara Cinema's premises, arising from any acts or omissions of Capybara Cinema, its staff and/or its other customers/patrons, whether those acts or omissions were negligent or otherwise.</li>
+<li>You agree to indemnify Capybara Cinema and/or its staff for any direct, indirect and/or consequential losses, damages, injuries suffered by Capybara Cinema, its staff and/or its other customers/patrons within Capybara Cinema's premises, arising from any of your actions or omissions, whether intentional, negligent or otherwise.</li>
+<li>Both you and Capybara Cinema agree that this contract is governed by laws of the Republic of Singapore, and both parties agree that all disputes are to be referred to arbitration.</li>
+</ol>
+<button class="btn-primary" type="button" onclick="window.location.href = 'index.php'">Back</button>
+</body>
+</html>
