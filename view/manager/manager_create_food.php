de93ad275c44de23a2e2de152ff4360e01fa6fc4
jooo576 <jonathan.chen72@gmail.com>
2023-05-15 20:30:32 +0800

    update

--- view/manager/manager_create_food.php
+++ view/manager/manager_create_food.php
@@ -78,19 +78,19 @@ require('../header.html');
     <h1>Food Details Form</h1>
 
     <label for="name">Name:</label>
-    <input type="text" name="foodName" required><br><br>
+    <input type="text" name="foodName" placeholder="Enter Name of Product" required><br><br>
 
     <label for="description">Description:</label>
-    <textarea name="description" rows="5"></textarea><br><br>
+    <textarea name="description" rows="5" placeholder="Enter Description"></textarea><br><br>
 
     <label for="price">Price:</label>
-    <input type="number" step="0.01" name="price" required><br><br>
+    <input type="number" step="0.01" name="price" placeholder="Enter Price " required min="0"><br><br>
 
     <label for="category">Category:</label>
-    <input type="text" name="category"><br><br>
+    <input type="text" name="category" placeholder="Enter Category" required><br><br>
 
     <label for="stock">Stock:</label>
-    <input type="number" name="stock" min="0" max="99999"><br><br>
+    <input type="number" name="stock" min="0" max="99999" placeholder="Enter Stock" required><br><br>
 
     <label for="image">Image:</label>
     <input type="text" name="image"><br>
