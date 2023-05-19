5af7b9be806a325251b3daeeddf950bd3896b929
jooo576 <jonathan.chen72@gmail.com>
2023-05-19 15:32:32 +0800

    update

--- view/admin_search_profile.php
+++ view/admin_search_profile.php
@@ -29,6 +29,10 @@ $controller = new admin_controller;
             margin-top: 200px;
             margin-left:0;
         }
+    .button-container {
+    display: flex;
+    justify-content: space-between;
+    }
 
     body {
       background-color: #e7dbd0;
@@ -70,28 +74,21 @@ $controller = new admin_controller;
       height: 100vh;
     }
 
-    /*
-    form {
-      width: 100%;
-      max-width: 500px;
-      margin-top:20px;
-      margin-right:350px;
-    }
-*/
+  
     .button-container {
       display: flex;
       justify-content: space-between;
     }
 
-    table {
-  height:100px;
-  width: 1300px;
-  border-collapse: collapse;
-  margin: 0 auto; /* Center the table horizontally */
-  margin-top: 10px; /* Add margin at the top for spacing between form and table */
-  margin-bottom: 20px;
-  margin-right:20px;
-}
+  table {
+    height:100px;
+    width: 1300px;
+    border-collapse: collapse;
+    margin: 0 auto; /* Center the table horizontally */
+    margin-top: 10px; /* Add margin at the top for spacing between form and table */
+    margin-bottom: 20px;
+    margin-right:20px;
+  }
 
 table th,
 table td {
@@ -124,13 +121,13 @@ table td {
 }
 
 .custom-button:hover {
-  background-color:#bd9a7a;
+  background-color:white;
   color: #bd9a7a;
   border: 1px solid #bd9a7a;
 }
 .custom-button a {
   text-decoration: none;
-  color: white;
+  color: black;
 }
 
   </style>
@@ -173,12 +170,16 @@ table td {
                               echo "<td>" . $array['password'] . "</td>";
                               echo "<td>" . $array['dob'] . "</td>";
                               echo "<td>" . $array['status'] . "</td>";
-                              echo '<td >
-                                  <button class="custom-button"><a href="../view/userUpdate.php?updateID='.$array['phone'].'&curProfile='.$searchProfile.'"
-                                  class="text-light">Update</a></button>
-                                  <button class="custom-button"><a href="../view/admin_home_view.php?deleteID='.$array['phone'].'&curProfile='.$searchProfile.'" class="text-light">Delete</a></
-                                  button> 
-                                  </td>' ; 
+                              echo '<td>
+                                    <div class="button-container">
+                                      <button class="custom-button">
+                                        <a href="../view/userUpdate.php?updateID='.$array['phone'].'&curProfile='.$searchProfile.'" class="text-light">Update</a>
+                                      </button>
+                                      <button class="custom-button">
+                                        <a href="../view/admin_home_view.php?deleteID='.$array['phone'].'&curProfile='.$searchProfile.'" class="text-light">Suspend</a>
+                                      </button>
+                                    </div>
+                                  </td>';
                               echo "</tr>";
                           }
                           
