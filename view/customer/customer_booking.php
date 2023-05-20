56174079433fb6326961e2729888705acbcf8859
jooo576 <jonathan.chen72@gmail.com>
2023-05-20 11:50:30 +0800

    update

--- view/customer/customer_booking.php
+++ view/customer/customer_booking.php
@@ -211,7 +211,7 @@ if ($selected_row === NULL){
 
                     <hr>
 
-                    <label for="Show"><b>Show Id</b></label>
+                    <label for="Show"><b>Show Time</b></label>
 
                     <div class="form-group">
                         <select class="form-control"  name="show_id"  id="show_id" style="border-radius:30px;">
@@ -227,7 +227,7 @@ if ($selected_row === NULL){
                     <label for="psw"><b>No. of Tickets</b></label>
                     <input type="number" style="border-radius:30px;" id="no_ticket" name="no_ticket"  required>
 
-                    <label for="psw-repeat"><b>Seat Deatils</b></label>
+                    <label for="psw-repeat"><b>Seat Details</Details></b></label>
                     <input type="text" style="border-radius:30px;" name="seat_dt" id="seat_dt" required>
 
                     <label for="psw"><b>No. of Adult Tickets</b></label>
